557480152d263323fa36ec8a96d9fb0b288dec67
Matthew Connelly <mattconndev@gmail.com>
2017-07-29 17:37:43 -0400

    copying file in main fn

--- src/main.c
+++ src/main.c
@@ -6,8 +6,14 @@
 #include "file-attributes.h"
 #include "util/reset-file-attr.h"
 #include "get-char-count.h"
+#include "copy-file.h"
 
 int main(int argc, char *argv[])
 {
+	char output[ get_char_count(argv[1]) ];
+	copy_file(argv[1], output);	
+
+	printf("%s\n", output);
+
 	return 0;
 }
