4001469067d09a32346f918bcd2d41f07d520f0f
Matthew Connelly <mattconndev@gmail.com>
2017-07-30 17:57:18 -0400

    Adding directive checking for loop in main func

--- src/main.c
+++ src/main.c
@@ -12,8 +12,23 @@ int main(int argc, char *argv[])
 {
 	char output[ get_char_count(argv[1]) ];
 	copy_file(argv[1], output);	
+	char direc[] = {"##INCLUDE"};
 
-	printf("%s\n", output);
+	bool has_direc = false;
+
+	for(int i = 0; i < sizeof(output)/sizeof(output[0]); i++)
+	{
+		if(output[i] == direc[0] && output[i+1] == direc[1])
+			has_direc = true;	
+	}
+
+	if(!has_direc)
+	{
+		printf("%s\n", "No directives found.");
+		return 1;
+	} else {
+		printf("%s\n", "Directives found.");
+	}
 
 	return 0;
 }
---
+++ src/util/arr_len.h
@@ -0,0 +1,4 @@
+int arr_len(char *arr)
+{
+	return sizeof(arr)/sizeof(arr[0]);
+}
