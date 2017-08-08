7cea8c3384aefcfc794873b6c4df1f55c62cf31f
Matthew Connelly <mattconndev@gmail.com>
2017-08-08 03:16:22 -0400

    Revision of help message naming to help notice

--- src/argument_validation.h
+++ src/argument_validation.h
@@ -3,12 +3,12 @@ bool arg_validation(int arg_count, char *arg_vector){
 	
 	bool valid = true;
 
-	char help_message[] = {"Try \"finc -h\" for usage/information."};
+	char help_notice[] = {"Try \"finc -h\" for usage/information."};
 
     // check for file argument
     if(arg_count < 2)
     {
-        printf("Missing file argument.\n%s\n", help_message);
+        printf("Missing file argument.\n%s\n", help_notice);
 		valid = false;
 		return valid;
     }
@@ -16,7 +16,7 @@ bool arg_validation(int arg_count, char *arg_vector){
     // check for valid file argument
     if( fopen(arg_vector, "r") == NULL  )
     {
-        printf("Invalid file argument.\n%s\n", help_message);
+        printf("Invalid file argument.\n%s\n", help_notice);
 		valid = false;
 		return valid;
     }
