976240870a43a309eebfb6dd222c24403dcec25f
Matthew Connelly <mattconndev@gmail.com>
2017-08-07 20:01:21 -0400

    fix directive trim fn paramter deletion (error from previous commit)

--- src/util/directive_trim.h
+++ src/util/directive_trim.h
@@ -1,3 +1,4 @@
+void direc_trim(char *str, char *directive, int size, char trimmed_dir_str[size] )
 {
 	// copy directive from str to trimmed dir str, from index <sizeof directive + 2 (for whitespace, next character)>
 	// to end of str (<len of str - len of directive>).
