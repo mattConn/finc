3c570b899e8f7cb6d0b1eaa81f4f94147b27f103
Matthew Connelly <mattconndev@gmail.com>
2017-08-07 20:58:45 -0400

    addition of help message to arg validation

--- src/argument_validation.h
+++ src/argument_validation.h
@@ -1,16 +1,18 @@
 // argument validation for use in main function
 void arg_validation(int arg_count, char *arg_vector){
+	char help_message[] = {"Try \"finc -h\" for usage/information."};
+
     // check for file argument
     if(arg_count < 2)
     {
-        printf("Missing file argument.\n");
+        printf("Missing file argument.\n%s\n", help_message);
 		exit(1);
     }
 
     // check for valid file argument
     if( fopen(arg_vector, "r") == NULL  )
     {
-        printf("Invalid file argument.\n");
+        printf("Invalid file argument.\n%s\n", help_message);
 		exit(1);
     }
 }
